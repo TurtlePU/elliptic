814da0424b12cc42f47227ae24474af97f0fa99f
TurtlePU <sokolov.p64@gmail.com>
2021-04-17 17:15:18 +0300

    curve points are now in projective coords

--- src/curve.rs
+++ src/curve.rs
@@ -22,6 +22,12 @@ pub fn check_char<F: Debug + Field>() {
     assert_ne!(F::one() * 3, F::zero());
 }
 
+pub fn check_curve<F: Debug + Field, C: Curve<F>>() {
+    check_char::<F>();
+    let prop = C::a().pow(3) * 4 + C::b().pow(2) * 27;
+    assert_ne!(prop, F::zero());
+}
+
 fn check_solution<F: Field, C: Curve<F>>(x: F, y: F) -> bool {
     y.pow(2) == x.clone().pow(3) + C::a() * x + C::b()
 }
@@ -30,43 +36,56 @@ fn check_solution<F: Field, C: Curve<F>>(x: F, y: F) -> bool {
 pub struct NotOnCurve;
 
 pub trait Points<F, C> {
-    fn projected(x: F, y: F) -> Result<EllipticPoint<F, C>, NotOnCurve>;
-
-    fn spatial(x: F, y: F, z: F) -> Result<EllipticPoint<F, C>, NotOnCurve>;
+    fn affine(x: F, y: F) -> Result<EllipticPoint<F, C>, NotOnCurve>;
 }
 
 impl<F: Field, C: Curve<F>> Points<F, C> for C {
-    fn projected(x: F, y: F) -> Result<EllipticPoint<F, C>, NotOnCurve> {
+    fn affine(x: F, y: F) -> Result<EllipticPoint<F, C>, NotOnCurve> {
         if check_solution::<F, C>(x.clone(), y.clone()) {
-            Ok(EllipticPoint::new(x, y))
+            Ok(EllipticPoint::new(x, y, F::one()))
         } else {
             Err(NotOnCurve)
         }
     }
-
-    fn spatial(x: F, y: F, z: F) -> Result<EllipticPoint<F, C>, NotOnCurve> {
-        let i = z.inv();
-        let x = x * i.clone();
-        let y = y * i;
-        Self::projected(x, y)
-    }
 }
 
 pub struct EllipticPoint<F, C> {
-    coords: Option<(F, F)>,
+    coords: (F, F, F),
     curve: PhantomData<C>,
 }
 
 impl<F, C> EllipticPoint<F, C> {
-    fn new(x: F, y: F) -> Self {
-        Self::from_coords(Some((x, y)))
+    fn new(x: F, y: F, z: F) -> Self {
+        Self::from_coords((x, y, z))
     }
 
-    fn from_coords(coords: Option<(F, F)>) -> Self {
+    fn from_coords(coords: (F, F, F)) -> Self {
         Self {
             coords,
             curve: PhantomData,
         }
+    }
+}
+
+impl<F: Clone, C> EllipticPoint<F, C> {
+    fn x(&self) -> F {
+        self.coords.0.clone()
+    }
+
+    fn y(&self) -> F {
+        self.coords.1.clone()
+    }
+
+    fn z(&self) -> F {
+        self.coords.2.clone()
+    }
+}
+
+impl<F: Field, C> From<EllipticPoint<F, C>> for (F, F) {
+    fn from(point: EllipticPoint<F, C>) -> Self {
+        let (x, y, z) = point.coords;
+        let i = z.inv();
+        (x * i.clone(), y * i)
     }
 }
 
@@ -78,34 +97,53 @@ impl<F: Clone, C> Clone for EllipticPoint<F, C> {
     }
 }
 
-impl<F: PartialEq, C> PartialEq for EllipticPoint<F, C> {
+impl<F: Field, C: Curve<F>> PartialEq for EllipticPoint<F, C> {
     fn eq(&self, other: &Self) -> bool {
-        self.coords == other.coords
+        if self.is_zero() {
+            other.is_zero()
+        } else {
+            self.x() * other.z() == other.x() * self.z()
+                && self.y() * other.z() == other.y() * self.z()
+        }
     }
 }
 
-impl<F: Eq, C> Eq for EllipticPoint<F, C> {}
+impl<F: Field, C: Curve<F>> Eq for EllipticPoint<F, C> {}
 
 impl<F: Field, C: Curve<F>> Add for EllipticPoint<F, C> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let c = |x: &F| x.clone();
-        match (self.coords, rhs.coords) {
-            (Some((xp, yp)), Some((xq, yq))) => {
-                if xp == xq && yp == -c(&yq) {
-                    return Self::zero();
-                }
-                let s = if xp != xq {
-                    (c(&yp) - yq) / (c(&xp) - c(&xq))
-                } else {
-                    (c(&xp).pow(2) * 3 + C::a()) / (c(&yp) + yq)
-                };
-                let xr = c(&s).pow(2) - c(&xp) - xq;
-                let yr = -yp + s * (xp - c(&xr));
-                Self::new(xr, yr)
-            }
-            (p, None) | (None, p) => Self::from_coords(p),
+        if self.is_zero() {
+            rhs
+        } else if rhs.is_zero() {
+            self
+        } else if self == -rhs.clone() {
+            Self::zero()
+        } else if self == rhs {
+            let q = self.y() * self.z() * 2;
+            let n = self.x().pow(2) * 3 + C::a() * self.z().pow(2);
+            let p = self.x() * self.y().pow(2) * self.z() * 4;
+            let u = n.clone().pow(2) - p.clone() * 2;
+
+            let x = u.clone() * q.clone();
+            let z = q.pow(3);
+            let y = n * (p - u) - self.y().pow(4) * self.z().pow(2) * 8;
+
+            Self::new(x, y, z)
+        } else {
+            let u = rhs.y() * self.z() - self.y() * rhs.z();
+            let v = rhs.x() * self.z() - self.x() * rhs.z();
+            let w = u.clone().pow(2) * self.z() * rhs.z()
+                - v.clone().pow(3)
+                - v.clone().pow(2) * 2 * self.x() * rhs.z();
+            let q = v.clone().pow(3) * self.y() * rhs.z();
+
+            let x = v.clone() * w.clone();
+            let z = self.z() * rhs.z() * v.clone().pow(3);
+            let y = u * (v.pow(2) * self.x() * rhs.z() - w) - q;
+
+            Self::new(x, y, z)
         }
     }
 }
@@ -121,11 +159,9 @@ impl<F: Field, C: Curve<F>> Sub for EllipticPoint<F, C> {
 impl<F: Field, C: Curve<F>> Neg for EllipticPoint<F, C> {
     type Output = Self;
 
-    fn neg(self) -> Self::Output {
-        match self.coords {
-            Some((x, y)) => Self::new(x, -y),
-            None => self,
-        }
+    fn neg(mut self) -> Self::Output {
+        self.coords.1 = -self.coords.1;
+        self
     }
 }
 
@@ -152,10 +188,10 @@ where
 
 impl<F: Field, C: Curve<F>> Zero for EllipticPoint<F, C> {
     fn zero() -> Self {
-        Self::from_coords(None)
+        Self::new(F::zero(), F::one(), F::zero())
     }
 
     fn is_zero(&self) -> bool {
-        self.coords.is_none()
+        self.coords.2.is_zero()
     }
 }
